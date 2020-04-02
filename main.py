d3f275b9d2e258468bc526ae570c78396a77c6be
abieler <andre.bieler81@gmail.com>
2020-04-02 11:35:34 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,53 @@
+import argparse
+import logging
+
+logging.basicConfig(level=logging.INFO)
+
+import spacy
+
+
+
+def replace_numberwords(text: str) -> str:
+    return " ".join([numbermapping.get(word.strip(".,:;"), word) for word in text.split(" ")])
+
+
+def preprocess(text: str) -> str:
+    text = text.replace(",", "").replace("\n", " ")
+    return replace_numberwords(text)
+
+
+def parse(text: str) -> dict:
+    result = {}
+    nlp = spacy.load("nlp")
+    doc = nlp(text)
+    for sent in doc.sents:
+        sdoc = nlp(sent.string.strip())
+        for ent in sdoc.ents:
+            try:
+                result[ent.label_] = int(ent.text.strip('.,'))
+            except ValueError:
+                logging.warning(f"Error parsing: {ent.text.strip('.,')}")
+    return result
+
+
+with open("numbers.txt", "r") as txtfile:
+    numbermapping = {l.strip(): str(i) for i, l in enumerate(txtfile.readlines())}
+numbermapping["hundert"] = "100"
+numbermapping["tausend"] = "1000"
+numbermapping["einen"] = "1"
+numbermapping["eine"] = "1"
+numbermapping["eins"] = "1"
+
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--filename", type=str)
+    args = parser.parse_args()
+
+    with open(args.filename, "r") as txtfile:
+        text = txtfile.read()
+
+    ptext = preprocess(text)
+    for k, v in parse(ptext).items():
+        print(f"{k:25}: {v}")
